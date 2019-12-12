523a2b808ee4af72ad3c351d9a88c7876c5429ad
Juan Saavedra <saavedra.juan.p@gmail.com>
2019-12-12 18:22:42 -0500

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let tString = ((f-32)*(5/9)).toFixed(1);
+  let cTemp = Number(tString);
+  return cTemp;
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let tString = ((c*(9/5))+32).toFixed(1);
+  let fTemp = Number(tString);
+  return fTemp;
 }
 
 module.exports = {
