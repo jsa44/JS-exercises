aa4072907f1038c31ba0fa8c14a6369973db1f1b
Juan Saavedra <saavedra.juan.p@gmail.com>
2019-12-12 18:10:53 -0500

    Complete leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    let isLeapYear;
+    if (x % 4 == 0) {
+        if (x % 100 == 0 ){
+            if (x % 400 == 0) {
+                isLeapYear = true;
+            }
+            else {
+                isLeapYear = false;
+            }
+        }
+        else {
+            isLeapYear = true;
+        }
+    }
+    else {
+        isLeapYear = false;
+    }
+    return isLeapYear;
 }
 
 module.exports = leapYears
